1bfc87a90b08175447f3c2c884d23f1c49ba34c2
ArhaanRSrivastava <102792365+ArhaanRSrivastava@users.noreply.github.com>
2022-08-11 15:47:39 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,186 @@
+namespace SpriteKind {
+    export const Level_clear = SpriteKind.create()
+}
+function clear_level () {
+    for (let value of sprites.allOfKind(SpriteKind.Player)) {
+        value.destroy()
+    }
+    for (let value of sprites.allOfKind(SpriteKind.Level_clear)) {
+        value.destroy()
+    }
+    level += 1
+    create_level()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Level_clear, function (sprite, otherSprite) {
+    clear_level()
+})
+function create_level () {
+    if (level == 1) {
+        tiles.setCurrentTilemap(tilemap`level5`)
+    } else if (level == 2) {
+        tiles.setCurrentTilemap(tilemap`level10`)
+    } else if (level == 3) {
+    	
+    } else if (level == 4) {
+    	
+    } else {
+    	
+    }
+    mySprite = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    tiles.placeOnRandomTile(mySprite, sprites.dungeon.collectibleInsignia)
+    controller.moveSprite(mySprite, 100, 100)
+    scene.cameraFollowSprite(mySprite)
+    for (let value of tiles.getTilesByType(assets.tile`myTile4`)) {
+        Level_clear = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Level_clear)
+        animation.runImageAnimation(
+        Level_clear,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . 5 5 5 5 . . . . . . 
+            . . . . . 5 5 5 5 5 5 . . . . . 
+            . . . . . 5 5 5 5 5 5 . . . . . 
+            . . . . . . 5 5 5 5 . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . 5 5 5 5 . . . . . . 
+            . . . . . 5 5 5 5 5 5 . . . . . 
+            . . . . 5 5 5 5 5 5 5 5 . . . . 
+            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+            . . . . 5 5 5 5 5 5 5 5 . . . . 
+            . . . . . 5 5 5 5 5 5 . . . . . 
+            . . . . . . 5 5 5 5 . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . 5 5 5 5 . . . . . . 
+            . . . . . 5 5 5 5 5 5 . . . . . 
+            . . . . 5 5 5 5 5 5 5 5 . . . . 
+            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+            . . . . 5 5 5 5 5 5 5 5 . . . . 
+            . . . . . 5 5 5 5 5 5 . . . . . 
+            . . . . . . 5 5 5 5 . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . 5 5 5 5 . . . . . . 
+            . . . . . 5 5 5 5 5 5 . . . . . 
+            . . . . . 5 5 5 5 5 5 . . . . . 
+            . . . . . . 5 5 5 5 . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `],
+        300,
+        true
+        )
+        tiles.placeOnTile(Level_clear, value)
+        tiles.setTileAt(value, assets.tile`myTile4`)
+    }
+}
+let Level_clear: Sprite = null
+let mySprite: Sprite = null
+let level = 0
+level = 1
+create_level()
+scaling.scaleByPercent(mySprite, -7, ScaleDirection.Uniformly, ScaleAnchor.Middle)
